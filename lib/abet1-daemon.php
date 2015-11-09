f15a1caf55d40bce60c7311df96a999277588f6a
Roger Gee <rpg11a@acu.edu>
2015-11-09 15:19:37 -0600

    Better daemon functionality

--- lib/abet1-daemon.php
+++ lib/abet1-daemon.php
@@ -8,16 +8,42 @@
         - accredited program status report notifications (digest)
 */
 
+////////////////////////////////////////////////////////////////////////////////
 // globals
+////////////////////////////////////////////////////////////////////////////////
+
 $STDIN = STDIN;
 $STDOUT = STDOUT;
 $STDERR = STDERR;
 $PIDFILE = null;
+$RESTART = false;
+$INCLUDES = array(
+    'abet1-query.php'
+);
 
 // constants
 define('APP_PREFIX',"abet1");
 define('LOG_FILE',"/var/log/abet1.log");
 define('ERROR_FILE',"/var/log/abet1.errors.log");
+
+////////////////////////////////////////////////////////////////////////////////
+// includes
+////////////////////////////////////////////////////////////////////////////////
+
+// include all required php sources; then grab file modification values
+set_include_path('/usr/lib/abet1:/usr/local/lib/abet1');
+$INCLUDE_MTIMES = array();
+foreach ($INCLUDES as $file) {
+    if ((include $file) === false)
+        fatal_error("could not include library '$file'");
+
+    $qfile = stream_resolve_include_path($file);
+    $INCLUDE_MTIMES[$qfile] = filemtime($qfile);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// functions
+////////////////////////////////////////////////////////////////////////////////
 
 // log_message() - log message to standard output; this should go to a log
 // file that the daemon creates
@@ -47,7 +73,8 @@ function daemonize() {
     global $PIDFILE;
     global $argv;
 
-    // become a daemon
+    // become a daemon: keep the current directory the same so that we can
+    // potentially restart ourself later
     $pid = pcntl_fork();
     if ($pid == -1)
         fatal_error("pcntl_fork() failed");
@@ -56,17 +83,10 @@ function daemonize() {
         if (posix_setsid() == -1)
             fatal_error("posix_setsid() failed");
 
-        // reset umask
-        umask(0);
-
-        // set working directory to root
-        if (!chdir('/'))
-            fatal_error("chdir() failed");
-
         // redirect STDOUT, STDIN and STDERR; we'll use PHP functionality
         // for this; handle each one separately so the system assigns the
         // correct descriptor; this will not work on Windows or with mod_php; on
-        // Linux the next available file descriptor number is used so we open
+        // Linux the next available f descriptor number is used so we open
         // them in order from STDIN to STDERR
         fclose(STDIN);
         fclose(STDOUT);
@@ -97,7 +117,7 @@ function daemonize() {
                 if (strlen(file_get_contents($PIDFILE)) > 0) {
                     fatal_error("the PID file already exists and is non-empty; "
                         . "this means another instance may be running; if not, "
-                        . "then delete the file $PIDFILE");
+                        . "then delete the file '$PIDFILE'");
                 }
             }
             $pf = @fopen($PIDFILE,'w');
@@ -114,6 +134,26 @@ function daemonize() {
     }
 }
 
+// check_include_files() - make sure our dependencies haven't been updated; if
+// so, then terminate (or restart) the daemon (i.e. ourself)
+function check_include_files() {
+    global $RESTART;
+    global $INCLUDE_MTIMES;
+
+    foreach ($INCLUDE_MTIMES as $qfile => $t) {
+        clearstatcache(true,$qfile); // this is annoying to have to call
+        $u = filemtime($qfile);
+        if ($t != $u) {
+            log_message("library file '$qfile' changed on disk; the daemon "
+                . "will restart");
+            $RESTART = true;
+            terminate();
+        }
+    }
+}
+
+// check_pidfile() - make sure pid file hasn't changed; if it has, then we
+// either terminate or restart ourself
 function check_pidfile() {
     global $PIDFILE;
 
@@ -135,21 +175,54 @@ function check_pidfile() {
 }
 
 function terminate($signo = -1) {
+    global $argv;
     global $PIDFILE;
-    log_message("process terminated");
+    global $RESTART;
+
+    if ($RESTART) {
+        // keep the pid file as it is; execve() will not change the process id
+
+        log_message("restarting this process");
+
+        // spawn new daemon instance that overrides this instance
+        if (!pcntl_exec(PHP_BINARY,$argv))
+            fatal_error("fail pcntl_exec() on process restart");
+
+        // control no longer in this process
+    }
 
     if (!is_null($PIDFILE)) {
         // delete pidfile
         unlink($PIDFILE);
     }
 
+    $signaldesc = "";
+    if ($signo != -1)
+        $signaldesc = "; received signal $signo";
+
+    log_message("this process will exit" . $signaldesc);
+    log_message("process terminated");
     exit(0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // main program operation //////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-daemonize(); // I normally don't approve of demonizing anyone
+
+$termfd = @fopen(posix_ctermid(),'r');
+if (is_resource($termfd)) {
+    // this process is hooked up to a terminal; become a daemon to detach
+    fclose($termfd);
+    daemonize(); // I normally don't approve of demonizing anyone
+}
+else {
+    // see if a pid file exists and is non-empty; if not, then become a daemon
+    if (!file_exists($PIDFILE) || file_get_contents($PIDFILE) == 0)
+        daemonize();
+    else // otherwise reuse pid file as an overwrite process
+        log_message("overwrite process started");
+}
+unset($termfd);
 
 // setup signal handlers
 declare(ticks = 1);
@@ -160,6 +233,7 @@ pcntl_signal(SIGPIPE,SIG_IGN);
 
 // enter into wait loop
 while (true) {
+    check_include_files();
     check_pidfile();
 
     // timeout for a minute
