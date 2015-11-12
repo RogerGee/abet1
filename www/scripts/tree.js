0075b2a5e5f41006b749118d9732071a68fda68c
Ethan Rutherford <ethanrutherford2@yahoo.com>
2015-11-12 09:41:41 -0600

    fix tree to support multiple sublevels more accurately

--- www/scripts/tree.js
+++ www/scripts/tree.js
@@ -1,14 +1,36 @@
 $(document).ready(function() {
 	$.when($(".tree ul").each(function() {
-		$(this).attr("h", $(this).css("height"));
+		var h = parseInt($(this).css("height"));
+		//set a dummy attribute h to the height of folded content
+		$(this).attr("h", h);
+		//subtract this height from all parent uls
+		$(this).parents(".tree ul").each(function() {
+			$(this).attr("h", $(this).attr("h") - h);
+		});
 	})).done(function() {
+		//init all to closed
 		$(".tree ul").css("height", 0);
 	});
 	$(".tree div").click(function() {
 		var ul = $($(this).parent()).children("ul");
-		if ($(ul).css("height") == "0px")
-			$(ul).css("height", $(ul).attr("h"));
-		else
+		if ($(ul).css("height") == "0px") {
+			var h = parseInt($(ul).attr("h"));
+			//open this ul
+			$(ul).css("height", h);
+			//and expand all parents by appropriate amount
+			$(ul).parents(".tree ul").each(function() {
+				$(this).css("height", parseInt($(this).css("height")) + h);
+			});
+		} else {
+			var h = parseInt($(ul).css("height"));
+			//cache the current height for reopening
+			$(ul).attr("h", h);
+			//close this ul
 			$(ul).css("height", 0);
+			//and shrink all parents by appropriate amount
+			$(ul).parents(".tree ul").each(function() {
+				$(this).css("height", parseInt($(this).css("height")) - h);
+			});
+		}
 	});
 });
